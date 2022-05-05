27d85ae4fffcd2309ac4bd0b3ce844c8c34e8108
bluemagma612 <bryce@bluemagma.com>
2022-05-04 23:51:45 -0500

    created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,28 +1,25 @@
-const { merge } = require('webpack-merge');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
+const { merge } = require("webpack-merge");
+
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 
 const devConfig = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: '/index.html'
-    }
+      index: "/index.html",
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        'marketing': 'marketing@http://localhost:8081/remoteEntry.js'
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
-      shared: ['react', 'react-dom']
+      shared: ["react", "react-dom"],
     }),
-    new HTMLWebpackPlugin({
-      template: './public/index.html',
-    })
-  ]
-}
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
