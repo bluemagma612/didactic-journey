3bd8668b579c62f38fd9543f6e8f4e82c29310b1
bluemagma612 <bryce@bluemagma.com>
2022-05-05 17:02:20 -0500

    trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <div>
-    <h1>Hello world</h1>
-    <hr />
-    <MarketingApp />
-  </div>
-
-}
+  return (
+    <div>
+      <h1>Hello world1</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
